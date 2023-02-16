536a17ebac4958bf9b8ea1313592630baea76d0f
Alisson-Ribeiro <alisson-ohkamy@hotmail.com>
2023-02-15 21:14:41 -0300

    anti-sql injection and minor fixes

--- delete.php
+++ delete.php
@@ -1,12 +1,19 @@
 <?php
 include "db_conn.php";
 $id = $_GET['id'];
-$sql = "DELETE FROM 'crud' WHERE id = $id";
+
+// Evita ataques de injeção de SQL
+$id = mysqli_real_escape_string($conn, $id);
+
+$sql = "DELETE FROM `crud` WHERE id = $id";
+
 $result = mysqli_query($conn, $sql);
+
 if($result){
+    // Redireciona para a página de listagem após excluir com sucesso
     header("Location: index.php?msg=Record deleted successfully");
 } else {
+    // Caso ocorra algum erro, exibe a mensagem de erro do MySQL
     echo "Failed: " . mysqli_error($conn);
 }
-
 ?>
