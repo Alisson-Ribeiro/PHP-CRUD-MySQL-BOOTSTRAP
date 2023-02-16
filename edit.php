bffec07d2ac85851f0393e958a1a1305d4fe8f0a
Alisson-Ribeiro <alisson-ohkamy@hotmail.com>
2023-02-15 21:06:15 -0300

    changes on edit.php fetching data

--- edit.php
+++ edit.php
@@ -1,27 +1,30 @@
 <?php
 
 include "db_conn.php";
+
 $id = $_GET['id'];
 
-
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
     $email = $_POST['email'];
     $gender = $_POST['gender'];
 
-
-    $sql = "UPDATE `crud` SET 'first_name'='$first_name','last_name'='$last_name','email'='$email', 'gender'='$gender' WHERE id=$id";
+    $sql = "UPDATE `crud` SET `first_name`='$first_name',`last_name`='$last_name',`email`='$email', `gender`='$gender' WHERE id=$id";
 
     $result = mysqli_query($conn, $sql);
 
-    if($result){
-        header("Location: index.php?msg=Data updated sucessfully");
-    } else{
+    if ($result) {
+        header("Location: index.php?msg=Data updated successfully");
+        exit;
+    } else {
         echo "Failed: " . mysqli_error($conn);
     }
 }
 
+$sql = "SELECT * FROM `crud` WHERE id = $id LIMIT 1";
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
 
 ?>
 
@@ -31,14 +34,13 @@ if(isset($_POST['submit'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous" defer></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
 
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    
     <title>PHP CRUD</title>
 </head>
 <body>
@@ -52,39 +54,28 @@ if(isset($_POST['submit'])){
             <p class="text-muted">Click update button after changing any information</p>
         </div>
 
-        <?php
-            $sql = "SELECT * FROM 'crud' WHERE id = $id LIMIT 1";
-            $result = mysqli_query($conn, $sql);
-            $row = mysqli_fetch_assoc($result);
-        ?>
-
         <div class="container d-flex justify-content-center">
             <form action="" method="POST" style="width:50vw; min-width:300px;">
                 <div class="row mb-3">
                     <div class="col">
                         <label class="form-label">First Name: </label>
-                        <input type="text" class="form-control" name="first_name" value="<?php
-                        echo $row['first_name']?>" >
-
+                        <input type="text" class="form-control" name="first_name" value="<?= $row['first_name'] ?>" >
                     </div>
                     <div class="col">
                         <label class="form-label">Last Name: </label>
-                        <input type="text" class="form-control" name="last_name" value="<?php
-                        echo $row['last_name']?>">
-
+                        <input type="text" class="form-control" name="last_name" value="<?= $row['last_name'] ?>">
                     </div>
                 </div>
                 <div class="mb-3">
                     <label class="form-label">Email: </label>
-                    <input type="email" class="form-control" name="email" value="<?php
-                        echo $row['email']?>">
+                    <input type="email" class="form-control" name="email" value="<?= $row['email'] ?>">
                 </div>
                 <div class="form-group mb-3">
                     <label>Gender: </label> &nbsp;
-                    <input type="radio" class="form-check-input" name="gender" id="male" value="male" <?php echo($row['gender']=='male')?"checked":""; ?>>
+                    <input type="radio" class="form-check-input" name="gender" id="male" value="male" <?= ($row['gender'] == 'male') ? "checked" : "" ?>>
                     <label for="male" class="form-input-label">Male</label>
                     &nbsp;
-                    <input type="radio" class="form-check-input" name="gender" id="female" value="female" <?php echo($row['gender']=='female')?"checked":""; ?>>
+                    <input type="radio" class="form-check-input" name="gender" id="female" value="female" <?= ($row['gender'] == 'female') ? "checked" : ""; ?>>
                     <label for="female" class="form-input-label">Female</label>
                 </div>
 
